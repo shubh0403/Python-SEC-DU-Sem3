23ff5aeddf4b6285503d0db7cd28b59565fc1cf9
Shubh Sharma <sharma.shubh0401@gmail.com>
2022-11-27 12:01:50 +0530

    Practical 10 added

---
+++ Codes/Practical10.py
@@ -0,0 +1,23 @@
+'''
+10. Write a function that takes a sentence as input from the user and calculates the frequency of each letter. 
+Use a variable of dictionary type to maintain the count.
+'''
+
+def calcFreqOfLetters(sentence):
+    freq = {}
+    for i in range(0, len(sentence), 1):
+        letter = sentence[i].lower()
+        if letter not in [' ', '.', ',', '\'', '\"', '!', ';']:
+            if letter in freq.keys():
+                freq[letter] += 1
+            else:
+                freq[letter] = 1
+    return freq
+
+if __name__ == "__main__":
+    sentence = input('\nEnter a sentence : ')
+    freq = calcFreqOfLetters(sentence)
+
+    print('\nFrequencies of each letter in the given sentence : ')
+    for letter in freq:
+        print('\'{}\' --> {}'.format(letter, freq[letter]))
