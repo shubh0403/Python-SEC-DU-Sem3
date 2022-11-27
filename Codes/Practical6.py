4ba4efceca2ec96f879316ec5f93c5b3a5747286
Shubh Sharma <sharma.shubh0401@gmail.com>
2022-11-27 13:16:17 +0530

    Practical 6 added

--- Codes/Practical6.py
+++ Codes/Practical6.py
@@ -2,7 +2,7 @@
 6. Consider a tuple t1={1,2,5,7,9,2,4,6,8,10}.
 Write a program to perform following operations:
 a) Print another tuple whose values are even numbers in the given tuple.
-b) Concatenate a tuplet2 = {11,13,15) with t1.
+b) Concatenate a tuple t2 = {11,13,15) with t1.
 c) Return maximum and minimum value from this tuple.
 """
 
