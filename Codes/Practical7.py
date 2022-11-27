930d1fc7f2dc13286bf963e59b4828b436360b94
Shubh Sharma <sharma.shubh0401@gmail.com>
2022-11-27 10:22:57 +0530

    Practical 7 added

---
+++ Codes/Practical7.py
@@ -0,0 +1,8 @@
+"""
+7. Write a menu driven program to perform the following on strings:
+a) Find the length of string.
+b) Return maximum of three strings.
+c) Accept a string and replace all vowels with “#”
+d) Find number of words in the given string.
+e) Check whether the string is a palindrome or not.
+"""
